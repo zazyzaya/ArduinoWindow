ebc458633aea229e6fd077e81512155abfdbb531
zazyzaya <zazyzaya@gmail.com>
2023-11-28 17:51:23 -0500

    Setup complete

---
+++ synthetic_window.ino
@@ -0,0 +1,15 @@
+// SunPosition - Version: Latest 
+// https://github.com/GyverLibs/SunPosition
+#include <SunPosition.h>
+
+// FastLED - Version: Latest 
+// https://github.com/FastLED/FastLED
+#include <FastLED.h>
+
+void setup() {
+    
+}
+
+void loop() {
+    
+}
